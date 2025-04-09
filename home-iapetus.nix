28c9fd3cbae4b78ec2647bad3a3b8946bde99bbf
Aria Edmonds <aria@ar1as.space>
2025-04-09 19:04:32 +0800

    switch to iosevka nerd font

--- home-iapetus.nix
+++ home-iapetus.nix
@@ -8,33 +8,11 @@
     vivaldi
     fuzzel
 
-    iosevka
-    nerd-fonts.symbols-only
-    (stdenv.mkDerivation {
-      inherit (nerd-fonts.symbols-only) version;
-      pname = "nerd-font-fontconfig";
-      src = fetchurl {
-        url = "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/v${nerd-fonts.symbols-only.version}/10-nerd-font-symbols.conf";
-        hash = "sha256-E95Bcx58kZrHa/wELcroZXY6WzDgFBQlOCXROZr39c8=";
-      };
-      dontUnpack = true;
-      dontConfigure = true;
-      dontBuild = true;
-      installPhase = ''
-        runHook preInstall
-
-        fontconfigdir="$out/etc/fonts/conf.d"
-        install -d "$fontconfigdir"
-        install "$src" "$fontconfigdir/10-nerd-font-symbols.conf"
-
-        runHook postInstall
-      '';
-      enableParallelBuilding = true;
-    })
+    nerd-fonts.iosevka
   ];
 
   fonts.fontconfig.enable = true;
-  fonts.fontconfig.defaultFonts.monospace = [ "Iosevka" ];
+  fonts.fontconfig.defaultFonts.monospace = [ "Iosevka Nerd Font Mono" ];
 
   programs.waybar = {
     enable = true;
